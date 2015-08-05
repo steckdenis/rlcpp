d6e07dc9a4a4278eb6bdbd74b319c48d889f3add
Denis Steckelmacher <steckdenis@yahoo.fr>
2015-08-05 11:06:47 +0200

    Make PerceptronModel learn faster and more stably
    Now that TanhActivation learns correctly, PerceptronModel has to be more careful
    not to overfit data.

--- model/nnetmodel.cpp
+++ model/nnetmodel.cpp
@@ -7,10 +7,10 @@
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -104,7 +104,7 @@ void NnetModel::learn(const std::vector<Episode *> &episodes)
     }
 
     // Train the network on that data
-    _network->train(inputs, outputs, weights, 1, 10, true);
+    _network->train(inputs, outputs, weights, 10, 4, true);
 }
 
 void NnetModel::vectorToVector(const std::vector<float> &stl, Vector &eigen)
